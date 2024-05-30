885e661ee9bc731baa23c9cc66927e37640dbd86
Resmit Dholariya <resmit.dholariya.prsnl@gmail.com>
2024-05-30 09:54:49 +0530

    changed / route

--- app.js
+++ app.js
@@ -82,6 +82,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
+
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
