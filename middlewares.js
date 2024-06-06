9133ed92a78a6de3c06a28156611995690dc5716
Resmit Dholariya <111690724+resmit-dholariya@users.noreply.github.com>
2024-06-07 03:25:09 +0530

    Update middlewares.js

--- middlewares.js
+++ middlewares.js
@@ -6,7 +6,7 @@ const ExpressError = require("./utils/ExpressError");
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.redirectUrl = req.originalUrl;
-    req.flash("error", "You must be logged in to perform this task!");
+    req.flash("error", "You must be Logged in!");
     return res.redirect("/login");
   }
   next();
